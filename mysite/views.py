12a98d90447677f1495bda906a1685db94fac3f0
CJCU-CC <bcatsddpoq@gmail.com>
2021-03-16 11:44:23 +0800

    2021/3/16

---
+++ mysite/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import render
+
+# Create your views here.
+def homepage(request,testmode):
+    html = "<h2>Hello World!{}</h2>".format(testmode)
+    return HttpResponse(html)
+# def about(request):
+#     html = "<h2>Here is Author's about page!</h2><hr>"
+#     return HttpResponse(html)
+def listing(request):
+    html = "<h2>Simple List</h2><hr>"
+    return HttpResponse(html)
+def about(request, author_no = 0):
+    html = "<h2>Here is Author:{}'s about page!</h2><hr>".format(author_no)
+    return HttpResponse(html)
+def listing(request, yr, mon, day):
+    html = "<h2>List Date is {}/{}/{}</h2><hr>".format(yr, mon, day)
+    return HttpResponse(html)
+def post01(request, yr, mon, day, post_num):
+    html = "<a href='{}'>Show the Post</a>".format(reverse('post-url', args=(yr, mon, day, post_num,)))
+    return HttpResponse(html)
+def post(request, yr, mon, day, post_num):
+    html = "<h2>{}/{}/{}:Post Number:{}</h2><hr>".format(yr, mon, day, post_num)
+    return HttpResponse(html)
